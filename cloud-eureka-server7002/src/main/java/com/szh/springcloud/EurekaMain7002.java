61772c7be95880f275d5874361f901b251b8cc28
张起灵-小哥 <64594975+2656307671@users.noreply.github.com>
2021-07-30 13:32:22 +0800

    Update EurekaMain7002.java

--- cloud-eureka-server7002/src/main/java/com/szh/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/szh/springcloud/EurekaMain7002.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  *
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //声明该模块是Eureka的服务端
 public class EurekaMain7002 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7002.class,args);
